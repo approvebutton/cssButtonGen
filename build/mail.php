e040ab3b3b023ee2ac08e5cd6d4ef2e3b48ebe9c
Alexander Polovnikov <aleksandrpol@me.com>
2014-09-01 11:57:29 +0400

    Add PHPMailer for send code

---
+++ build/mail.php
@@ -0,0 +1,29 @@
+<?php
+
+    if($_POST) {
+    require("lib/mail/PHPMailerAutoload.php");
+
+    $mail = new PHPMailer();
+
+    $mail->IsSMTP();
+    $mail->Host       = "smtp.gmail.com";
+    $mail->SMTPAuth   = true;
+    $mail->SMTPSecure = "ssl";
+    $mail->Port       = 465;
+    $mail->Charset    = "UTF-8";
+    $mail->Username   = "alpha7@sredatv.ru";
+    $mail->Password   = "alpha123";
+    $mail->From       = "alpha7@sredatv.ru";
+    $mail->AddAddress("{$_POST['email']}");
+    $mail->Subject    = 'CSS3 Button';
+    $mail->Body     = "HTML: \r\n {$_POST['html']}\r\n CSS: \r\n {$_POST['css']}";
+
+    if(!$mail->Send()) {
+      echo 'Message was not sent.';
+      echo 'Mailer error: ' . $mail->ErrorInfo;
+    } else {
+      echo 'Message has been sent.';
+    };
+};
+
+?>
